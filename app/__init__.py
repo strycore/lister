2295a435246fa162d31ad8cbb24fc3a59c03921e
Mathieu Comandon <strycore@gmail.com>
2020-06-12 14:59:09 -0700

    Add view to list folder contents

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,21 @@ NOTES_DIR = os.environ.get("NOTES_DIR")
 app = Flask(__name__)
 CORS(app)
 
+
+@app.route("/<folder>", methods=["GET"])
+def list_folder(folder):
+    """Lists the contents of a folder as return it as plain text"""
+    folderpath = os.path.join(NOTES_DIR, folder)
+    if os.path.exists(folderpath):
+        content = "\n".join(os.listdir(folderpath))
+    else:
+        content = ""
+    return Response(content, mimetype="application/text")
+
+
 @app.route("/<folder>/<filename>", methods=["GET"])
 def read_file(folder, filename):
+    """Read the contents of a file and return it as plain text"""
     filepath = os.path.join(NOTES_DIR, folder, filename)
     if os.path.exists(filepath):
         with open(filepath, "r") as infile:
@@ -21,6 +34,7 @@ def read_file(folder, filename):
 
 @app.route("/<folder>/<filename>", methods=["POST"])
 def write_file(folder, filename):
+    """Write raw POST data to a text file"""
     with open(os.path.join(NOTES_DIR, folder, filename), "wb") as outfile:
         outfile.write(request.data)
     return Response("", mimetype="application/text")
